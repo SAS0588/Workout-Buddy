161876f403daabcd7c703acbb83d2cefb45f8da2
SAS0588 <48109100+SAS0588@users.noreply.github.com>
2019-07-22 22:18:23 -0500

    Finished exercise list functionality
    Created a conditional for checking form data.  Created a function for creating list elements to post exercise, number, and type values. Created an eventListener for clear button. Created a function for clear button.

--- js/script.js
+++ js/script.js
@@ -1,28 +1,59 @@
+// form validation check for numbers in input field
+const checkAgainst = /^[0-9]+$/;
+
+// Add eventListeners for exercise submission and clear list
+document.getElementById('submit').addEventListener("click",submit);
+document.getElementById('clear').addEventListener("click",clear);
 
 // Grab text and store in in a variable
 function submit(){
+    // set values to variables
     const exercise = document.getElementById('exercise').value;
     const number = document.getElementById('number').value;
     const type = document.getElementById('type-of-exercise').value;
+    console.log(`${exercise},${number}`);
+
+    // erase UI input values
     document.getElementById('exercise').value = '';
-    console.log(exercise + " " + number + " " + type);
-    //console.log(type);
-    // create a list item
+    document.getElementById('number').value = '';
+    document.getElementById('type-of-exercise').value = 'Reps';
+
+    // Check form for accuracy
+    if (exercise.length === 0){
+        alert("please enter an exercise.");
+    } else if(number.length === 0 || number == 'e'){
+        alert("please check the number field for an error.");
+    } else if (exercise.match(checkAgainst)) {
+        alert("please enter a proper exercise name");
+    } else {
+        // pass variables to createElement function
+        createElement(exercise,number,type);
+    };
+};
+
+function createElement(ex,num,typ){
+    // 1. create a list item and span item
     const listItem = document.createElement('li');
     const spanItem = document.createElement('span')
-    // create a text node to attach the text value to the list item
-    const listItemText = document.createTextNode(exercise);
-    const spanItemText = document.createTextNode(number + " " + type);
-    // attach the text to the list item
+    // 2. create span attribute
+    const spanAttribute = document.createAttribute('class');
+    spanAttribute.value = 'span-item-list';
+    spanItem.setAttributeNode(spanAttribute);
+    // 3. create a text node to attach the text value to the list item and span item
+    const listItemText = document.createTextNode(ex);
+    const spanItemText = document.createTextNode(` ${num} ${typ}`);
+    // 4. attach the text to the list item and span item
     spanItem.appendChild(spanItemText);
     listItem.appendChild(listItemText);
-    console.log(listItem);
+    // 5. attach the span item to the end of list item
     listItem.append(spanItem);
-    console.log(listItem);
-    //spanItemText.appendChild(spanItemText);
-    // attach span item to list item
-    // get the parent node: unordered list
+    // 6. get the parent node: unordered list
     unorderedList = document.getElementById('exercise-list-unordered');
-    // attach the new list item + the exercise text to the parent
+    // 7. attach the new list item + the exercise text to the parent
     unorderedList.appendChild(listItem);
+};
+
+function clear(){
+    // this function clears the entire list of exercises
+    console.log('clear function works');
 }
