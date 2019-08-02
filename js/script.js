f7262554cdc215b29e91e4ab4e9abcd906e19094
ssharieff <saadsharieff@gmail.com>
2019-08-01 21:13:48 -0500

    BIG functionality changes
    Countdown timer works. I deleted a bunch of spaghetti code and made new functions to handle the state of the application. I cleaned up round timer and now it is looking prettier than before. Countdown timer needs some work to make it look pretty, but it works beautifully.
    
    a few bugs found. especially number input takes in negative numbers, so we gotta fix that.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
-// form validation check for numbers in input field
-const checkAgainst = /^[0-9]+$/;
+////////////////////////////////////////////
+///////////  Exercise  ////////////////////
+//////////////////////////////////////////
 
 // Add eventListeners for exercise submission and clear list
 document.getElementById('submit').addEventListener("click",submit);
 document.getElementById('clear').addEventListener("click",clear);
+
+// form validation check for numbers in input field
+const checkAgainst = /^[0-9]+$/;
 
 // Grab text and store in in a variable
 function submit(){
@@ -22,7 +26,7 @@ function submit(){
         alert("please enter an exercise.");
     } else if(number.length === 0 || number == 'e'){
         alert("please check the number field for an error.");
-    } else if (exercise.match(checkAgainst)) {
+    } else if (exercise.match(/^[0-9]+$/)) {
         alert("please enter a proper exercise name");
     } else {
         // pass variables to createElement function
@@ -57,10 +61,34 @@ function clear(){
     console.log('clear function works');
 };
 
+
 ////////////////////////////////////////////
 ///////////    Timers  ////////////////////
 //////////////////////////////////////////
 
+// Toggling between Round Timer and Countdown Timer
+
+// Event Listener for Round or Countdown button
+document.getElementById('rounds').addEventListener('click',toggleRoundOn);
+document.getElementById('countdown').addEventListener('click',toggleCountdownOn);
+
+// Toggle Functions
+function toggleRoundOn() {
+    document.getElementById('countdown-display').style.display = 'none';
+    document.getElementById('minutes-input').value = '';
+    document.getElementById('seconds-input').value = '';
+    document.getElementById('rounds-display').style.display = 'block';
+};
+
+function toggleCountdownOn(){
+    document.getElementById('countdown-display').style.display = 'block';
+    document.getElementById('rounds-display').style.display = 'none';
+    document.getElementById('number-of-rounds').value = '';
+};
+
+/*
+Round Timer Functions
+*/
 
 /* Algorithm:
         1. create a function that executes for every second using setInterval.
@@ -70,41 +98,47 @@ function clear(){
         5. check if the seconds reach 0 if true then alert user and clear the timer.
     */
 
-/*
-Round Timer Functions
-*/
-
 // Event Listener
-document.getElementById('submit-rounds-timer').addEventListener('click', main);
+document.getElementById('submit-rounds-timer').addEventListener('click', formCheck);
 document.getElementById('clear-rounds-timer').addEventListener('click', stopInterval);
-document.getElementById('rounds').addEventListener('click',hideCountdown);
 
-// Default View
-document.getElementById('countdown-display').style.display = 'none';
-
-// Global Variables
-let lengthOfRounds;
-let numOfRounds;
+// Form Check
+function formCheck(){
+    const roundInput = document.getElementById('number-of-rounds').value;
+    if(roundInput.length === 0 || roundInput == 'e'){
+        alert("please check the number of rounds field for an error.");
+    } else {
+        main();
+    };
+};
 
 // Start Round Functionality
 function main(){
 
-    // Display Properties
+    // Immediately turn off the submit button and Countdown button since round timer has started.
     document.getElementById('submit-rounds-timer').disabled = true;
     document.getElementById('countdown').disabled = true;
-    document.getElementById('round-display').style.display = 'block';
-    document.getElementById('timer-display').style.display = 'block';
-    document.getElementById('finished-display').innerHTML = '';
-    document.getElementById('round-display').innerHTML = '';
-    document.getElementById('timer-display').innerHTML = '';
 
-    // String => Number conversion
-    lengthOfRounds = Number(document.getElementById('length-of-rounds').value);
-    numOfRounds = Number(document.getElementById('number-of-rounds').value);
+    // Display Properties
+    setDisplay();
+
+    // String to Number conversion
+    let lengthOfRounds = Number(document.getElementById('length-of-rounds').value);
+    let numOfRounds = Number(document.getElementById('number-of-rounds').value);
 
     // Start intervals and pass the length of each round and the number of each round
     roundTimerInterval(lengthOfRounds, numOfRounds);
 };
+
+function setDisplay(){
+    // show the displays
+    document.getElementById('round-display').style.display = 'block';
+    document.getElementById('timer-display').style.display = 'block';
+    // make the display blank
+    document.getElementById('finished-display').innerHTML = '';
+    document.getElementById('round-display').innerHTML = '';
+    document.getElementById('timer-display').innerHTML = '';
+}
 
 // @param ln = length of rounds & num = number of rounds
 function roundTimerInterval(ln,num){
@@ -130,39 +164,34 @@ function roundTimerInterval(ln,num){
             } else {
                 console.log('Timer has finished!');
                 stopInterval();
+                finishedDisplay();
+                endDisplay();
             };
         };
-        document.getElementById('round-display').innerHTML = `Round: ${number}`;
+        document.getElementById('round-display').innerHTML = `Round: ${number} of ${num}`;
     },1000);
 };
 
-// Stop Round Timer
+// Stop Round Timer and enable buttons
 function stopInterval(){
     clearInterval(intervalVariable);
     document.getElementById('submit-rounds-timer').disabled = false;
     document.getElementById('countdown').disabled = false;
-    document.getElementById('round-display').style.display = 'none';
-    document.getElementById('timer-display').style.display = 'none';
-    document.getElementById('finished-display').innerHTML = `Timer has finished!`;
 };
 
-function hideCountdown(){
-    document.getElementById('rounds-display').style.display = 'block';
-    document.getElementById('countdown-display').style.display = 'none';
-};
+function finishedDisplay(){
+    document.getElementById('finished-display').innerHTML = 'Timer has finished!';
+}
+
+function endDisplay(){
+    document.getElementById('round-display').style.display = 'none';
+    document.getElementById('timer-display').style.display = 'none';
+    document.getElementById('finished-display').style.display = 'block';
+}
 
 ///////////////////////////////////////
 //    Countdown Timer Functions     //
 /////////////////////////////////////
-
-// Event listener for Countdown button
-document.getElementById('countdown').addEventListener('click',hideRounds);
-
-// Hides all round displays/functionality
-function hideRounds(){
-    document.getElementById('rounds-display').style.display = 'none';
-    document.getElementById('countdown-display').style.display = 'block';
-};
 
 // Start, Stop, and Reset event listeners
 document.getElementById('start-countdown').addEventListener('click',startCountdown);
@@ -176,29 +205,40 @@ function startCountdown(){
         4. decrement the seconds by “1" for each second.
         5. check if the seconds reach 0 if true then alert user and clear the timer.
     */
-   const hours = Number(document.getElementById('hours-input').value);
    const mins = Number(document.getElementById('minutes-input').value);
    const secs = Number(document.getElementById('seconds-input').value);
-   countdownTimerInterval(hours,mins,secs);
-
+   countdownTimerInterval(mins,secs);
 }
 
-function countdownTimerInterval(h,m,s){
-    let total = (h * 3600) + (m * 60) + (s);
-    console.log(total);
+function countdownTimerInterval(m,s){
+    let total = (m * 60) + (s);
     
-
     // executes every second
     countdownIntervalVariable = setInterval(() => {
         
-         // Decrease time by seconds
+         // Decrease total time and seconds by one
         total = total - 1;
-        console.log(total);
-        // Check if timer is finished
-        if(total === 3661){
-            stopCountdown();
+        s = s - 1;
+
+        // Check if seconds have run out
+        if (s < 0){
+            m = m - 1;
+            s = 59;
         };
 
+        // Check if seconds are in single digits
+        if(s < 10){
+            s = '0' + s;
+        };
+
+        // Update view
+        document.getElementById('countdown-clock-display').innerHTML = `${m}:${s}`;
+
+        // Check if countdown is finished
+        if (total === 0){
+            stopCountdown();
+        };
+        
     }, 1000);
 };
 
@@ -206,7 +246,12 @@ function stopCountdown(){
     clearInterval(countdownIntervalVariable);
 };
 
+// NEXT STEPS:
+/*
+Fix countdown timer's functionality. We need to be able to disable rounds button, check field input (especially negative input), and apply reset button functionality.
 
+Find bugs (rounds still has an ability to take negative numbers) and squash them!
+*/
 
 
 
